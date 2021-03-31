a778c7aefaf92066a08a7d2d5a83f703f9e4f0cc
christianturner <christiandavidphoto@gmail.com>
2021-03-31 16:33:56 -0400

    create albums functionality

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from "vuex";
 import { auth } from "./auth/auth";
+import { albumInfo } from "./albums/albums.js";
 
 export default createStore({
-  modules: { auth },
+  modules: { auth, albumInfo },
 });
